2acc66d6576c87f8719fad55a2b39f9445370675
Sith <sith@zuluhotel.ca>
2016-01-30 22:28:33 -0500

    disable AOS, UO:R

--- Scripts/Misc/CurrentExpansion.cs
+++ Scripts/Misc/CurrentExpansion.cs
@@ -5,7 +5,7 @@ namespace Server
 {
 	public class CurrentExpansion
 	{
-		private static readonly Expansion Expansion = Expansion.HS;
+		private static readonly Expansion Expansion = Expansion.T2A;
 
 		public static void Configure()
 		{
