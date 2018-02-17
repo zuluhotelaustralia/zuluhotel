61485fafdb9ed451515dc2322d58999810b4ad5a
Daleron <daleron@zuluhotel.ca>
2018-02-16 22:32:04 -0500

    Implement SummonMammal

--- Server/Scripts/Spells/Earth/SummonMammalsSpell.cs
+++ Server/Scripts/Spells/Earth/SummonMammalsSpell.cs
@@ -3,40 +3,40 @@ using System.Collections;
 using Server.Network;
 using Server.Items;
 using Server.Targeting;
+using Server.Mobiles;
 
 namespace Server.Spells.Earth
 {
     public class SummonMammalsSpell : AbstractEarthSpell
     {
         private static SpellInfo m_Info = new SpellInfo(
-                "Summon Mammals", "Chame O Mamifero Agora"
-                );
+                                                        "Summon Mammals", "Chame O Mamifero Agora",
+                                                        16, false,
+                                                        typeof( SerpentsScales ),
+                                                        typeof( PigIron ),
+                                                        typeof( EyeOfNewt ));
 
-        public override TimeSpan CastDelayBase { get { return TimeSpan.FromSeconds( 0 ); } }
+        private static Type[] m_Mammals = {
+            typeof ( GreyWolf ),
+            typeof ( TimberWolf ),
+            typeof ( Horse ),
+            typeof ( Couger ),
+            typeof ( Panther ),
+            typeof ( BrownBear ),
+            typeof ( GrizzlyBear ),
+            typeof ( ForestOstard )
+        };
 
-        public override double RequiredSkill{ get{ return 0.0; } }
-        public override int RequiredMana{ get{ return 0; } }
+        public override TimeSpan CastDelayBase { get { return TimeSpan.FromSeconds( 4 ); } }
 
-        public SummonMammalsSpell( Mobile caster, Item scroll ) : base( caster, scroll, m_Info )
-        {
-        }
+        public override double RequiredSkill{ get{ return 60.0; } }
+        public override int RequiredMana{ get{ return 5; } }
+
+        public SummonMammalsSpell( Mobile caster, Item scroll ) : base( caster, scroll, m_Info ) { }
 
         public override void OnCast()
         {
-            Caster.Target = new InternalTarget( this );
-        }
-
-        public void Target( Mobile m )
-        {
-            if ( ! Caster.CanSee( m ) )
-            {
-                // Seems like this should be responsibility of the targetting system.  --daleron
-                Caster.SendLocalizedMessage( 500237 ); // Target can not be seen.
-                goto Return;
-            }
-
-            if ( ! CheckSequence() )
-            {
+            if ( ! CheckSequence() ) {
                 goto Return;
             }
 
@@ -44,63 +44,28 @@ namespace Server.Spells.Earth
                 goto Return;
             }
 
-            SpellHelper.Turn( Caster, m );
+            double effectiveness = SpellHelper.GetEffectiveness( Caster );
 
-            // TODO: Spell graphical and sound effects.
+            int count = (int)(3 * effectiveness);
 
-            Caster.DoHarmful( m );
+            if ( Caster.Spec.SpecName == SpecName.Mage ) {
+                count++;
+                bonus = 1;
+            }
 
-            // TODO: Spell action ( buff/debuff/damage/etc. )
+            for ( int i = 0 ; i < count ; i++ ) {
+                double roll = 0.8 * Utility.RandomDouble() + 0.2 * effectiveness;
+                int mammal = (int)Math.Max(m_Mammals.length - 1,
+                                           Math.Floor(m_Mammals.length * roll));
 
-            new InternalTimer( m, Caster ).Start();
+                BaseCreature creature = (BaseCreature)Activator.CreateInstance( m_Mammals[mammal] );
+                TimeSpan duration = TimeSpan.FromSeconds((int)(5 * 60 * effectiveness));
+
+                SpellHelper.Summon( creature, Caster, 0x215, duration, false, false );
+            }
 
         Return:
             FinishSequence();
         }
-
-        private class InternalTimer : Timer
-        {
-            private Mobile m_Target;
-
-            public InternalTimer( Mobile target, Mobile caster ) : base( TimeSpan.FromSeconds( 0 ) )
-            {
-                m_Target = target;
-
-                // TODO: Compute a reasonable duration, this is stolen from ArchProtection
-                double time = caster.Skills[SkillName.Magery].Value * 1.2;
-                if ( time > 144 )
-                    time = 144;
-                Delay = TimeSpan.FromSeconds( time );
-                Priority = TimerPriority.OneSecond;
-            }
-
-            protected override void OnTick()
-            {
-                m_Target.EndAction( typeof( SummonMammalsSpell ) );
-            }
-        }
-
-        private class InternalTarget : Target
-        {
-            private SummonMammalsSpell m_Owner;
-
-            // TODO: What is thie Core.ML stuff, is it needed?
-            public InternalTarget( SummonMammalsSpell owner ) : base( Core.ML ? 10 : 12, false, TargetFlags.Harmful )
-            {
-                m_Owner = owner;
-            }
-
-            protected override void OnTarget( Mobile from, object o )
-            {
-                if ( o is Mobile )
-                    m_Owner.Target( (Mobile) o );
-            }
-
-            protected override void OnTargetFinish( Mobile from )
-            {
-                m_Owner.FinishSequence();
-            }
-        }
-
     }
 }
