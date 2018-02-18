62352ed2efa508dbe400730d925aeb4ef06a503d
Sith <sith@zuluhotel.ca>
2018-02-17 18:29:19 -0800

    implement Rising Fire

--- Server/Scripts/Spells/Earth/RisingFireSpell.cs
+++ Server/Scripts/Spells/Earth/RisingFireSpell.cs
@@ -10,12 +10,14 @@ namespace Server.Spells.Earth
     {
         private static SpellInfo m_Info = new SpellInfo(
                 "Rising Fire", "Batida Do Fogo"
+		233, 9012,
+		Reagent.Batwing, Reagent.Brimstone, Reagent.VialOfBlood
                 );
 
         public override TimeSpan CastDelayBase { get { return TimeSpan.FromSeconds( 0 ); } }
 
-        public override double RequiredSkill{ get{ return 0.0; } }
-        public override int RequiredMana{ get{ return 0; } }
+        public override double RequiredSkill{ get{ return 100.0; } }
+        public override int RequiredMana{ get{ return 15; } }
 
         public RisingFireSpell( Mobile caster, Item scroll ) : base( caster, scroll, m_Info )
         {
@@ -44,16 +46,42 @@ namespace Server.Spells.Earth
                 goto Return;
             }
 
-            SpellHelper.Turn( Caster, m );
+	    double range = 3.0;
+	    if( Caster is PlayerMobile &&
+		((PlayerMobile)Caster).Spec.SpecName != SpecName.None &&
+		((PlayerMobile)Caster).Spec.SpecName != SpecName.PowerPlayer ){
 
-            // TODO: Spell graphical and sound effects.
+		range *= ((PlayerMobile)Caster).Spec.Bonus;
+	    }
+	    
+	    List<Mobile> targets = new List<Mobile>();
+	    Map map = Caster.Map;
 
-            Caster.DoHarmful( m );
+	    if( map != null){
+		foreach ( Mobile m in Caster.GetMobilesInRange( (int)range )){
+		    if ( Caster != m &&
+			 SpellHelerp.ValidIndirectTarget( Caster, m ) &&
+			 Caster.CanBeHarmful( m, false ) &&
+			 Caster.InLOS( m )){
 
-            // TODO: Spell action ( buff/debuff/damage/etc. )
+			targets.Add(m);
+		    }
+		}
+	    }
 
-            new InternalTimer( m, Caster ).Start();
+	    Caster.PlaySound( 0x208 );
 
+	    for( int i=0; i< targets.Count; i++){
+		Mobile m = targets[i];
+
+		//original spell deals something like (24*3)d5+(caster's magery / 5)*0.5 to all targets
+		// for an average roll of 243 damage per anydice.com
+		double dmg = Caster.Skills[DamageSkill].Value / 6.0;
+		Caster.DoHarmful(m);
+		m.FixedParticles( 0x3709, 10, 30, 5052, EffectLayer.LeftFoot );
+		m.Damage((int)dmg, Caster, DamageType.Fire);
+	    }
+	    
         Return:
             FinishSequence();
         }
