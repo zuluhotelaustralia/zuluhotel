b68ca57133cabe6affe58452762ba701eb009801
Sith <sith@zuluhotel.ca>
2019-04-27 20:27:43 -0400

    increase amount of bandages for sale on tailors

--- Server/Scripts/Mobiles/Vendors/SBInfo/SBTailor.cs
+++ Server/Scripts/Mobiles/Vendors/SBInfo/SBTailor.cs
@@ -20,7 +20,7 @@ namespace Server.Mobiles
 		{
 			public InternalBuyInfo()
 			{
-				Add( new GenericBuyInfo( typeof( Bandage ), 5, 50, 0xE21, 0 ) ); 
+				Add( new GenericBuyInfo( typeof( Bandage ), 5, 999, 0xE21, 0 ) ); 
 				Add( new GenericBuyInfo( typeof( SewingKit ), 3, 20, 0xF9D, 0 ) ); 
 				Add( new GenericBuyInfo( typeof( Scissors ), 11, 20, 0xF9F, 0 ) );
 				Add( new GenericBuyInfo( typeof( DyeTub ), 8, 20, 0xFAB, 0 ) ); 
