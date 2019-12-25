c59c4136ddd7bbe96dad218c765c1463f1f001dd
Sith <sith@zuluhotel.ca>
2019-12-25 02:00:33 -0500

    LOGS ARE ALL FUCKED

--- Server/Scripts/Items/Skill Items/Lumberjack/Generated/SilverleafLog.cs
+++ Server/Scripts/Items/Skill Items/Lumberjack/Generated/SilverleafLog.cs
@@ -1,32 +1,32 @@
 // Generated File. DO NOT MODIFY BY HAND.
 namespace Server.Items {
 
-	public class SilverleafLog : Log {
-		[Constructable]
-		public SilverleafLog() : this( 1 ) {}
+    public class SilverleafLog : Log {
+	[Constructable]
+	public SilverleafLog() : this( 1 ) {}
 
-		[Constructable]
-		public SilverleafLog( int amount ) : base( CraftResource.Silverleaf, amount ) {
-			this.Hue = 2301;
-		}
+	[Constructable]
+	public SilverleafLog( int amount ) : base( CraftResource.Silverleaf, amount ) {
+	    this.Hue = 2301;
+	}
 
-		public SilverleafLog( Serial serial ) : base( serial ) {}
+	public SilverleafLog( Serial serial ) : base( serial ) {}}
+	
+	public override void Serialize( GenericWriter writer ) {
+	    base.Serialize( writer );
+	    writer.Write( (int) 0 ); // version
+	}
 
-		public override void Serialize( GenericWriter writer ) {
-			base.Serialize( writer );
-			writer.Write( (int) 0 ); // version
-		}
+	public override void Deserialize( GenericReader reader ) {
+	    base.Deserialize( reader );
+	    int version = reader.ReadInt();
+	}
 
-		public override void Deserialize( GenericReader reader ) {
-			base.Deserialize( reader );
-			int version = reader.ReadInt();
-		}
-
-		public override bool Axe( Mobile from, BaseAxe axe ) {
-			if ( !TryCreateBoards( from,  110 , new SilverleafBoard() ) ) {
-				return false;
-			}
-			return true;
-		}
-	}	
+	public override bool Axe( Mobile from, BaseAxe axe ) {
+	    if ( !TryCreateBoards( from,  110 , new SilverleafBoard() ) ) {
+		return false;
+	    }
+	    return true;
+	}
+    }	
 }
