3e19a14c3a3895aa86adfbdb6015434e0b099d5c
Sith <sith@zuluhotel.ca>
2019-12-26 22:34:36 -0500

    vendors don't sell boats any more

--- Server/Scripts/Mobiles/Vendors/SBInfo/SBProvisioner.cs
+++ Server/Scripts/Mobiles/Vendors/SBInfo/SBProvisioner.cs
@@ -84,7 +84,7 @@ namespace Server.Mobiles
 				Add( new GenericBuyInfo( typeof( Bedroll ), 5, 20, 0xA59, 0 ) );
 				Add( new GenericBuyInfo( typeof( Kindling ), 2, 20, 0xDE1, 0 ) );
 
-				Add( new GenericBuyInfo( "1041205", typeof( Multis.SmallBoatDeed ), 10177, 20, 0x14F2, 0 ) );
+				//Add( new GenericBuyInfo( "1041205", typeof( Multis.SmallBoatDeed ), 10177, 20, 0x14F2, 0 ) );
 
 				Add( new GenericBuyInfo( "1041060", typeof( HairDye ), 60, 20, 0xEFF, 0 ) );
 
