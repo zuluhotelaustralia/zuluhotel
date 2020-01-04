fec11979a76b1bbed47aaf260e6cc287f8d7c99f
Sith <sith@zuluhotel.ca>
2020-01-04 16:12:05 -0500

    this commit is broken do not compile

--- Server/Scripts/Spec/Spec.cs
+++ Server/Scripts/Spec/Spec.cs
@@ -1,6 +1,9 @@
 using System;
-using Server;
 using System.Text;
+
+using Server;
+using Server.Accounting;
+using Server.Targeting;
 using Server.Commands;
 using Server.Spells;
 
@@ -24,10 +27,33 @@ namespace Server.Mobiles
 	public static void Initialize(){
 	    CommandSystem.Register( "spec", AccessLevel.Player, new CommandEventHandler( Spec_OnCommand ) );
 	    CommandSystem.Register( "showclasse", AccessLevel.Player, new CommandEventHandler( Spec_OnCommand ) );
+
+	    SetMaximums();
+	}
+
+	private class InternalTarget : Target {
+	    public InternalTarget() : base( 12, false, TargetFlags.None ){}
+
+	    protected override void OnTarget( Mobile from, object targ ){
+		if( targ is PlayerMobile ){
+		    PlayerMobile pm = targ as PlayerMobile;
+		    from.SendMessage("{0}: {1}, level {2}", pm.Name, pm.Spec.SpecName, pm.Spec.SpecLevel);
+		}
+		else {
+		    from.SendMessage("Can only target players.");
+
+		}
+	    }
 	}
 
 	public static void Spec_OnCommand( CommandEventArgs e ){
 	    PlayerMobile pm = e.Mobile as PlayerMobile;
+
+	    if( pm.AccessLevel >= AccessLevel.Counselor ){
+		pm.Target = new InternalTarget();
+		return;
+	    }
+	    
 	    pm.Spec.ComputeSpec();
 
 	    if( pm.Spec.SpecName == SpecName.None ){
@@ -105,12 +131,146 @@ namespace Server.Mobiles
 	    }
 	}
     
-	//pulled this BS from zh scandinavia
-	private const int m_SKILLPOINTSPERLEVEL = 120;
-	private const int m_SKILLBASE = 480;
-	private const double m_PERCENTPERLEVEL = 0.08;
-	private const double m_PERCENTBASE = 0.52;
+	//reference original ZH Canada (ZH3) release
+	private const double m_ClassPointsPerLevel = 120;
+	private const double m_SkillBase = 480;
+	private const double m_PercentPerLevel = 0.08;
+	private const double m_PercentBase = 0.52;
 
+	private static double[] m_MinSkills;
+	private static double[] m_MaxSkills;
+	    
+	public void ComputeSpec()
+	{
+	    m_SpecName = SpecName.None;
+	    m_SpecLevel = 0;
+		
+	    //minimum on-class skill points per level are 600 for level 1, then += 120 pts per level
+	    //maximum total skill points per level are 1000 for level 1, then ( 0.52 + 0.08(level) ) * minskill
+	    // if you fail the maxskill check then you are still eligible for the next spec down.
+	    // average on-class skill must be satisfied for spec.
+
+	    double total = (double)m_ClassSkills.Total;
+	    total *= 0.1;
+
+	    if( total < 600.0 ){
+		return;
+	    }
+
+	    if( total >= 3920.0 ){
+		m_SpecName = SpecName.Powerplayer;
+		m_SpecLevel = 1;
+		
+		if( total >= 5145 ){
+		    m_SpecLevel = 2;
+		    
+		    if( total >= 6370 ){
+			m_SpecLevel = 3;
+		    }
+		}
+		//we're a pp so:
+		return;
+	    }
+
+	    int maxlevel = GetMaxLevel( total );
+	    if( maxlevel < 0 ){
+		return;
+	    }
+	    if ( maxlevel > 5 ){
+		maxlevel = 5;
+	    }
+
+	    double thiefSkills = m_ClassSkills.Hiding.Value +
+		m_ClassSkills.Snooping.Value +
+		m_ClassSkills.Stealing.Value +
+		m_ClassSkills.Stealth.Value +
+		m_ClassSkills.DetectHidden.Value +
+		m_ClassSkills.RemoveTrap.Value +
+		m_ClassSkills.Lockpicking.Value +
+		m_ClassSkills.Poisoning.Value;
+
+	    double bardSkills = m_ClassSkills.Begging.Value + 
+		m_ClassSkills.Cartography.Value +
+		m_ClassSkills.Discordance.Value +
+		m_ClassSkills.Herding.Value + 
+		m_ClassSkills.Musicianship.Value + 
+		m_ClassSkills.Peacemaking.Value + 
+		m_ClassSkills.Provocation.Value +
+		m_ClassSkills.TasteID.Value;
+
+	    double crafterSkills = m_ClassSkills.ArmsLore.Value + 
+		m_ClassSkills.Blacksmith.Value +
+		m_ClassSkills.Fletching.Value +
+		m_ClassSkills.Carpentry.Value +
+		m_ClassSkills.Lumberjacking.Value +
+		m_ClassSkills.Mining.Value +
+		m_ClassSkills.Tailoring.Value +
+		m_ClassSkills.Tinkering.Value;
+
+	    double mageSkills = m_ClassSkills.Alchemy.Value +
+		m_ClassSkills.EvalInt.Value +
+		m_ClassSkills.Inscribe.Value +
+		m_ClassSkills.ItemID.Value +
+		m_ClassSkills.Magery.Value +
+		m_ClassSkills.Meditation.Value +
+		m_ClassSkills.MagicResist.Value +
+		m_ClassSkills.SpiritSpeak.Value;
+
+	    double rangerSkills = m_ClassSkills.AnimalLore.Value +
+		m_ClassSkills.AnimalTaming.Value +
+		m_ClassSkills.Camping.Value +
+		m_ClassSkills.Cooking.Value +
+		m_ClassSkills.Fishing.Value +
+		m_ClassSkills.Tracking.Value +
+		m_ClassSkills.Archery.Value +
+		m_ClassSkills.Veterinary.Value;
+
+	    double warriorSkills = m_ClassSkills.Anatomy.Value +
+		m_ClassSkills.Fencing.Value +
+		m_ClassSkills.Healing.Value +
+		m_ClassSkills.Macing.Value +
+		m_ClassSkills.Parry.Value +
+		m_ClassSkills.Swords.Value +
+		m_ClassSkills.Tactics.Value +
+		m_ClassSkills.Wrestling.Value;   
+
+	    if( bardSkills >= m_MinSkills[maxlevel] ){
+		m_SpecLevel = maxlevel;
+		m_SpecName = SpecName.Bard;
+	    }
+	    else if ( bardSkills >= m_MinSkills[maxlevel - 1] ){
+	}
+
+	private int GetMaxLevel( double total ){
+	    int i = 0;
+	    foreach( double d in m_MaxSkills ){
+		if( d >= total ){
+		    i++;
+		}
+	    }
+
+	    return i;
+	}
+	
+	private static void SetMaximums() {
+	    // max skill = minskill * (0.52 + 0.08*level)
+
+	    m_MinSkills = new double[6];
+
+	    for( int i=0; i<6; i++ ){
+		m_MinSkills[i] = m_SkillBase + ( m_ClassPointsPerLevel * (double)i );
+	    }
+	    
+	    m_MaxSkills = new double[6];
+
+	    for( int i=0; i<6; i++ ){
+	        m_MaxSkills[i] = Math.Floor( m_MinSkills[i] / ( m_PercentBase + ( m_PercentPerLevel * (double)i )) );
+	    }
+
+	    Console.WriteLine("Minimums: [ {0}, {1}, {2}, {3}, {4}, {5} ]", m_MinSkills[0], m_MinSkills[1], m_MinSkills[2], m_MinSkills[3], m_MinSkills[4], m_MinSkills[5]);
+	    Console.WriteLine("Maximums: [ {0}, {1}, {2}, {3}, {4}, {5} ]", m_MaxSkills[0], m_MaxSkills[1], m_MaxSkills[2], m_MaxSkills[3], m_MaxSkills[4], m_MaxSkills[5]);	    
+	}	    
+	    
 	//constructor needs a reference to the parent playermobile obj.
 	public Spec(PlayerMobile parent)
 	{
@@ -232,217 +392,5 @@ namespace Server.Mobiles
 		    }
 	    }
 	}
-	
-	public void ComputeSpec()
-	{
-	    //keep in mind this has to run every time anyone swings a weapon or uses a skill so it needs to avoid looping if possible
-	    //also note that total is in fixed-point aka "x10" notation, whereas skillname.value is in regular decimal form
-
-	    double total = m_ClassSkills.Total / 10;
-
-	    //thief TODO incomplete.
-	    double thiefSkills = m_ClassSkills.Hiding.Value;
-
-	    //bard
-	    double bardSkills = m_ClassSkills.Begging.Value + 
-		m_ClassSkills.Cartography.Value +
-		m_ClassSkills.Discordance.Value +
-		m_ClassSkills.Herding.Value + 
-		m_ClassSkills.Musicianship.Value + 
-		m_ClassSkills.Peacemaking.Value + 
-		m_ClassSkills.Provocation.Value +
-		m_ClassSkills.TasteID.Value;
-
-	    //crafter
-	    double crafterSkills = m_ClassSkills.ArmsLore.Value + 
-		m_ClassSkills.Blacksmith.Value +
-		m_ClassSkills.Fletching.Value +
-		m_ClassSkills.Carpentry.Value +
-		m_ClassSkills.Lumberjacking.Value +
-		m_ClassSkills.Mining.Value +
-		m_ClassSkills.Tailoring.Value +
-		m_ClassSkills.Tinkering.Value;
-
-	    //mage
-	    double mageSkills = m_ClassSkills.Alchemy.Value +
-		m_ClassSkills.EvalInt.Value +
-		m_ClassSkills.Inscribe.Value +
-		m_ClassSkills.ItemID.Value +
-		m_ClassSkills.Magery.Value +
-		m_ClassSkills.Meditation.Value +
-		m_ClassSkills.MagicResist.Value +
-		m_ClassSkills.SpiritSpeak.Value;
-
-	    //ranger
-	    double rangerSkills = m_ClassSkills.AnimalLore.Value +
-		m_ClassSkills.AnimalTaming.Value +
-		m_ClassSkills.Camping.Value +
-		m_ClassSkills.Cooking.Value +
-		m_ClassSkills.Fishing.Value +
-		m_ClassSkills.Tracking.Value +
-		m_ClassSkills.Archery.Value +
-		m_ClassSkills.Veterinary.Value;
-
-	    //warrior
-	    double warriorSkills = m_ClassSkills.Anatomy.Value +
-		m_ClassSkills.Fencing.Value +
-		m_ClassSkills.Healing.Value +
-		m_ClassSkills.Macing.Value +
-		m_ClassSkills.Parry.Value +
-		m_ClassSkills.Swords.Value +
-		m_ClassSkills.Tactics.Value +
-		m_ClassSkills.Wrestling.Value;
-
-	    // tl;dr whoever wrote the original function did not consider performance
-	    // extremely messy include/skills.inc from POL contains horrifiying isfromthatclasse() function that can be significantly
-	    //reduced if you take the time to actually figure out wtf is going on
-
-	    // floor( (specskills - 480) / 120 ) gives level
-	    // enforce specskills/total < 0.6 + 0.08*level to prevent excess offclass skills
-	    // check if they are pp
-	    // enforce level >= 0
-	    // if less than 600 skill points they cannot be spec
-
-	    if(total < 600.0)
-	    {
-		m_SpecName = SpecName.None;
-		m_SpecLevel = 0;
-		return;
-	    }
-      
-	    if(total>=6370.0)
-	    {
-		m_SpecLevel = 3;
-		m_SpecName = SpecName.Powerplayer;
-		return;
-	    }
-	    else
-	    {
-		if(total>=5145.0)
-		{
-		    m_SpecLevel = 2;
-		    m_SpecName = SpecName.Powerplayer;
-		    return;
-		}
-		else
-		{
-		    if(total>=3920.0)
-		    {
-			m_SpecLevel = 1;
-			m_SpecName = SpecName.Powerplayer;
-			return;
-		    }
-		}
-	    }
-
-	    //bard calcs
-	    int bardLevel = ( (int)bardSkills - m_SKILLBASE) / m_SKILLPOINTSPERLEVEL;
-	    if(bardLevel < 0)
-	    {
-		bardLevel = 0;
-	    }
-	    if( (bardSkills/total) < (m_PERCENTBASE + m_PERCENTPERLEVEL * (double)bardLevel ))
-	    {
-		//on-spec skills must comprise 60% of total at level1, and an additional 8% for each level after
-		// if this evals true, then they have too many off-spec skills (on-spec proportion is too small)
-		bardLevel = 0; //should this be bardlevel--; ?
-	    }
-	    if(bardLevel > 0)
-	    {
-		m_SpecLevel = bardLevel;
-		m_SpecName = SpecName.Bard;
-	    }
-      
-	    //crafter cals
-	    int crafterLevel = ( (int)crafterSkills - m_SKILLBASE) / m_SKILLPOINTSPERLEVEL;
-	    if(crafterLevel < 0)
-	    {
-		crafterLevel = 0;
-	    }
-	    if( (crafterSkills/total) < (m_PERCENTBASE + m_PERCENTPERLEVEL * (double)crafterLevel ))
-	    {
-		//on-spec skills must comprise 60% of total at level1, and an additional 8% for each level after
-		// if this evals true, then they have too many off-spec skills (on-spec proportion is too small)
-		crafterLevel = 0; //should this be crafterlevel--; ?
-	    }
-	    if(crafterLevel > 0)
-	    {
-		m_SpecLevel = crafterLevel;
-		m_SpecName = SpecName.Crafter;
-	    }
-      
-	    //mage calcs
-	    int mageLevel = ( (int)mageSkills - m_SKILLBASE) / m_SKILLPOINTSPERLEVEL;
-	    if(mageLevel < 0)
-	    {
-		mageLevel = 0;
-	    }
-	    if( (mageSkills/total) < (m_PERCENTBASE + m_PERCENTPERLEVEL * (double)mageLevel ))
-	    {
-		//on-spec skills must comprise 60% of total at level1, and an additional 8% for each level after
-		// if this evals true, then they have too many off-spec skills (on-spec proportion is too small)
-		mageLevel = 0; //should this be magelevel--; ?
-	    }
-	    if(mageLevel > 0)
-	    {
-		m_SpecLevel = mageLevel;
-		m_SpecName = SpecName.Mage;
-	    }
-      
-	    //ranger calcs
-	    int rangerLevel = ( (int)rangerSkills - m_SKILLBASE) / m_SKILLPOINTSPERLEVEL;
-	    if(rangerLevel < 0)
-	    {
-		rangerLevel = 0;
-	    }
-	    if( (rangerSkills/total) < (m_PERCENTBASE + m_PERCENTPERLEVEL * (double)rangerLevel ))
-	    {
-		//on-spec skills must comprise 60% of total at level1, and an additional 8% for each level after
-		// if this evals true, then they have too many off-spec skills (on-spec proportion is too small)
-		rangerLevel = 0; //should this be rangerlevel--; ?
-	    }
-	    if(rangerLevel > 0)
-	    {
-		m_SpecLevel = rangerLevel;
-		m_SpecName = SpecName.Ranger;
-	    }
-      
-	    //thief calcs
-	    int thiefLevel = ( (int)thiefSkills - m_SKILLBASE) / m_SKILLPOINTSPERLEVEL;
-	    if(thiefLevel < 0)
-	    {
-		thiefLevel = 0;
-	    }
-	    if( (thiefSkills/total) < (m_PERCENTBASE + m_PERCENTPERLEVEL * (double)thiefLevel ))
-	    {
-		//on-spec skills must comprise 60% of total at level1, and an additional 8% for each level after
-		// if this evals true, then they have too many off-spec skills (on-spec proportion is too small)
-		thiefLevel = 0; //should this be thieflevel--; ?
-	    }
-	    if(thiefLevel > 0)
-	    {
-		m_SpecLevel = thiefLevel;
-		m_SpecName = SpecName.Thief;
-	    }
-      
-	    //warrior calcs
-	    int warriorLevel = ( (int)warriorSkills - m_SKILLBASE) / m_SKILLPOINTSPERLEVEL;
-	    if(warriorLevel < 0)
-	    {
-		warriorLevel = 0;
-	    }
-	    if( (warriorSkills/total) < (m_PERCENTBASE + m_PERCENTPERLEVEL * (double)warriorLevel ))
-	    {
-		//on-spec skills must comprise 60% of total at level1, and an additional 8% for each level after
-		// if this evals true, then they have too many off-spec skills (on-spec proportion is too small)
-		warriorLevel = 0; //should this be warriorlevel--; ?
-	    }
-	    if(warriorLevel > 0)
-	    {
-		m_SpecLevel = warriorLevel;
-		m_SpecName = SpecName.Warrior;
-	    }
-	} //computespec
-
     } //spec
 }
