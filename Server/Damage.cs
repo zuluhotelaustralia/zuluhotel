011e218720c62859b6a3d5f08098be538b78c35d
Sith <sith@zuluhotel.ca>
2020-01-10 20:58:39 -0500

    add explanatory comments

--- Server/Damage.cs
+++ Server/Damage.cs
@@ -1,6 +1,8 @@
 using Server;
 
 namespace Server {
+
+//DAMAGETYPE is the elemental damage type (we're bypassing AOS's bullshit)
     public enum DamageType {
 	None,
 	Air,
@@ -11,6 +13,11 @@ namespace Server {
 	Poison
     }
 
+    //ATTACKTYPE is the actual type of attack e.g. shooting a guy with a bow vs casting a spell
+    //Differentiating these things in this manner allows for e.g. elemental fire bows doing
+    // ranged fire damage, or magical attacks (spells) that do different elemental damages
+    // but still lets us handle spec-based bonuses in an elegant way
+    
     public enum AttackType {
 	Raw,
 	Physical,
