eb534c8a38c1399dd65c063440d9c23aa5025a4b
Sith <sith@zuluhotel.ca>
2020-01-10 22:26:07 -0500

    add a pagan reagent vendor

---
+++ Server/Scripts/Items/Suits/PaganVendorSuit.cs
@@ -0,0 +1,33 @@
+using System;
+using Server;
+
+namespace Server.Items
+{
+    public class PaganVendorSuit : BaseSuit
+    {
+	[Constructable]
+	public PaganVendorSuit() : base( AccessLevel.GameMaster, 2749, 0x204E )
+	{
+	    this.Name = "a tattered robe";
+	}
+
+	public PaganVendorSuit( Serial serial ) : base( serial )
+	{
+	    this.Name = "a tattered robe";
+	}
+
+	public override void Serialize( GenericWriter writer )
+	{
+	    base.Serialize( writer );
+
+	    writer.Write( (int) 0 ); // version
+	}
+
+	public override void Deserialize( GenericReader reader )
+	{
+	    base.Deserialize( reader );
+
+	    int version = reader.ReadInt();
+	}
+    }
+}
