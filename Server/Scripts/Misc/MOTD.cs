74d561b2b7acb4111a24fd81af9bd5a0185665f6
Sith <sith@zuluhotel.ca>
2020-01-15 21:35:33 -0500

    update MOTD

--- Server/Scripts/Misc/MOTD.cs
+++ Server/Scripts/Misc/MOTD.cs
@@ -34,13 +34,14 @@ namespace Server.Gumps {
 	    Resizable = false;
 
 	    AddPage(0);
-	    AddBackground( 0, 0, 480, 320, 9300 );
-	    AddHtml( 10, 10, 460, 100, "<h2>Update: 14 January 2020</h2>", false, false );
-	    AddHtml( 10, 50, 460, 140, "Welcome to Zulu Hotel Canada, brought to you by Sith and Daleron.  This shard is currently in Beta Testing, and so may have bugs.  We ask that you please be patient with us.  Current testing priority is crafting:  Please try to craft everything you can, and report bugs that you find!  Note that we have updated new clilocs for the client, however Daleron's updater is not currently ready for production, so we have put the cliloc file up on https://zuluhotel.ca for download.  Latest feature change:  Spec modifier is now 10% per level.  Known issue:  Crafters are not getting spec bonuses right now.  A fix is in the works but that system is complex so there's lots to test.", false, false );
+	    AddBackground( 0, 0, 480, 480, 9300 );
+	    AddHtml( 10, 10, 460, 100, "<h2>Update: 15 January 2020</h2>", false, false );
+	    AddHtml( 10, 50, 460, 140, "Welcome to Zulu Hotel Canada, brought to you by Sith and Daleron.  This shard is currently in Beta Testing, and so may have bugs.  We ask that you please be patient with us.  Current testing priority is crafting:  Please try to craft everything you can, and report bugs that you find!  Note that we have updated new clilocs for the client, however Daleron's updater is not currently ready for production, so we have put the cliloc file up on zuluhotel.ca for download.", false, false );
+	    AddHtml( 10, 200, 460, 140, "Latest feature changes:  Spec modifier is now 10% per level.  Alchemists now receive spec bonuses.  Warriors now receive spec bonus to AR.  Known issue(s):  Crafters are not getting spec bonuses right now.  A fix is in the works but that system is very complex so there's lots to test.", false, false );
 	    
-	    AddButton( 10, 280, 247, 248, (int)Buttons.OKButton, GumpButtonType.Reply, 2);
-	    AddImageTiled( 10, 280, 68, 22, 2624);
-	    AddLabel( 20, 282, 49, @"Okay");
+	    AddButton( 10, 440, 247, 248, (int)Buttons.OKButton, GumpButtonType.Reply, 2);
+	    AddImageTiled( 10, 440, 68, 22, 2624);
+	    AddLabel( 20, 442, 49, @"Okay");
 	}
 
 	public override void OnResponse( NetState state, RelayInfo info ){
