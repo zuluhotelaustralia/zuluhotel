5d1a178cb0d2414fa220b0ecc48dd31a86ff59a0
Sith <sith@zuluhotel.ca>
2020-04-14 15:09:19 -0400

    enable AOS Custom Housing

--- Server/Scripts/Mobiles/Vendors/SBInfo/SBArchitect.cs
+++ Server/Scripts/Mobiles/Vendors/SBInfo/SBArchitect.cs
@@ -4,37 +4,35 @@ using Server.Items;
 
 namespace Server.Mobiles
 {
-	public class SBArchitect : SBInfo
+    public class SBArchitect : SBInfo
+    {
+	private List<GenericBuyInfo> m_BuyInfo = new InternalBuyInfo();
+	private IShopSellInfo m_SellInfo = new InternalSellInfo();
+
+	public SBArchitect()
 	{
-		private List<GenericBuyInfo> m_BuyInfo = new InternalBuyInfo();
-		private IShopSellInfo m_SellInfo = new InternalSellInfo();
-
-		public SBArchitect()
-		{
-		}
-
-		public override IShopSellInfo SellInfo { get { return m_SellInfo; } }
-		public override List<GenericBuyInfo> BuyInfo { get { return m_BuyInfo; } }
-
-		public class InternalBuyInfo : List<GenericBuyInfo>
-		{
-			public InternalBuyInfo()
-			{
-				Add( new GenericBuyInfo( "1041280", typeof( InteriorDecorator ), 10001, 20, 0xFC1, 0 ) );
-				if ( Core.AOS )
-					Add( new GenericBuyInfo( "1060651", typeof( HousePlacementTool ), 627, 20, 0x14F6, 0 ));
-			}
-		}
-
-		public class InternalSellInfo : GenericSellInfo
-		{
-			public InternalSellInfo()
-			{
-				Add( typeof( InteriorDecorator ), 5000 );
-
-				if ( Core.AOS )
-					Add( typeof( HousePlacementTool ), 301 );
-			}
-		}
 	}
+
+	public override IShopSellInfo SellInfo { get { return m_SellInfo; } }
+	public override List<GenericBuyInfo> BuyInfo { get { return m_BuyInfo; } }
+
+	public class InternalBuyInfo : List<GenericBuyInfo>
+	{
+	    public InternalBuyInfo()
+	    {
+		Add( new GenericBuyInfo( "1041280", typeof( InteriorDecorator ), 10001, 20, 0xFC1, 0 ) );
+		Add( new GenericBuyInfo( "1060651", typeof( HousePlacementTool ), 627, 20, 0x14F6, 0 ));
+	    }
+	}
+
+	public class InternalSellInfo : GenericSellInfo
+	{
+	    public InternalSellInfo()
+	    {
+		Add( typeof( InteriorDecorator ), 5000 );
+
+		Add( typeof( HousePlacementTool ), 301 );
+	    }
+	}
+    }
 }
