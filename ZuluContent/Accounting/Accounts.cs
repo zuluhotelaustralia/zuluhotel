92d6db85d0084bae1c7e18a5ce12e8d93866bbc3
michaelwhelehan <michael.marula+1@gmail.com>
2021-02-25 14:17:15 +0100

    Upgrades to latest ModernUO

--- ZuluContent/Accounting/Accounts.cs
+++ ZuluContent/Accounting/Accounts.cs
@@ -5,91 +5,123 @@ using System.Xml;
 
 namespace Server.Accounting
 {
-  public class Accounts
-  {
-    private static Dictionary<string, IAccount> m_Accounts = new Dictionary<string, IAccount>();
-
-    static Accounts()
+    public class Accounts
     {
-    }
+        private static readonly Dictionary<string, IAccount>
+            _accountsByName = new(32, StringComparer.OrdinalIgnoreCase);
 
-    public static int Count => m_Accounts.Count;
+        private static Dictionary<Serial, IAccount> _accountsById = new(32);
+        private static Serial _lastAccount;
+        internal static List<Type> Types { get; } = new();
 
-    public static void Configure()
-    {
-      EventSink.WorldLoad += Load;
-      EventSink.WorldSave += Save;
-    }
+        private static void OutOfMemory(string message) => throw new OutOfMemoryException(message);
 
-    public static IEnumerable<IAccount> GetAccounts() => m_Accounts.Values;
-
-    public static IAccount GetAccount(string username)
-    {
-      m_Accounts.TryGetValue(username, out IAccount a);
-
-      return a;
-    }
-
-    public static void Add(IAccount a)
-    {
-      m_Accounts[a.Username] = a;
-    }
-
-    public static void Remove(string username)
-    {
-      m_Accounts.Remove(username);
-    }
-
-    public static void Load()
-    {
-      m_Accounts = new Dictionary<string, IAccount>(32, StringComparer.OrdinalIgnoreCase);
-
-      string filePath = Path.Combine("Saves/Accounts", "accounts.xml");
-
-      if (!File.Exists(filePath))
-        return;
-
-      XmlDocument doc = new XmlDocument();
-      doc.Load(filePath);
-
-      XmlElement root = doc["accounts"];
-
-      foreach (XmlElement account in root.GetElementsByTagName("account"))
-        try
+        public static Serial NewAccount
         {
-          new Account(account);
+            get
+            {
+                uint last = _lastAccount;
+
+                for (uint i = 0; i < uint.MaxValue; i++)
+                {
+                    last++;
+
+                    if (FindAccount(last) == null)
+                    {
+                        return _lastAccount = last;
+                    }
+                }
+
+                OutOfMemory("No serials left to allocate for accounts");
+                return Serial.MinusOne;
+            }
         }
-        catch
+
+        public static int Count => _accountsByName.Count;
+
+        public static void Configure() =>
+            Persistence.Register(Serialize, WriteSnapshot, Deserialize);
+
+        internal static void Serialize() =>
+            EntityPersistence.SaveEntities(_accountsById.Values, account => account.Serialize());
+
+        internal static void WriteSnapshot(string basePath)
         {
-          Console.WriteLine("Warning: Account instance load failed");
+            IIndexInfo<Serial> indexInfo = new EntityTypeIndex("Accounts");
+            EntityPersistence.WriteEntities(indexInfo, _accountsById, Types, basePath, out _);
+        }
+
+        public static IEnumerable<IAccount> GetAccounts() => _accountsByName.Values;
+
+        public static IAccount GetAccount(string username)
+        {
+            _accountsByName.TryGetValue(username, out var a);
+
+            return a;
+        }
+
+        public static void Add(IAccount a)
+        {
+            _accountsByName[a.Username] = a;
+            _accountsById[a.Serial] = a;
+        }
+
+        public static void Remove(IAccount a)
+        {
+            _accountsByName.Remove(a.Username);
+            _accountsById.Remove(a.Serial);
+        }
+
+        internal static void Deserialize(string path)
+        {
+            var filePath = Path.Combine(path, "Accounts", "accounts.xml");
+
+            // Backward Compatibility
+            if (File.Exists(filePath))
+            {
+                DeserializeXml(filePath);
+                return;
+            }
+
+            IIndexInfo<Serial> indexInfo = new EntityTypeIndex("Accounts");
+
+            _accountsById = EntityPersistence.LoadIndex(path, indexInfo, out List<EntityIndex<IAccount>> accounts);
+            EntityPersistence.LoadData(path, indexInfo, accounts);
+
+            foreach (var a in _accountsById.Values)
+            {
+                _accountsByName[a.Username] = a;
+            }
+        }
+
+        private static void DeserializeXml(string filePath)
+        {
+            var doc = new XmlDocument();
+            doc.Load(filePath);
+
+            var root = doc["accounts"];
+            if (root == null)
+            {
+                throw new FileLoadException("Unable to load xml file");
+            }
+
+            foreach (XmlElement account in root.GetElementsByTagName("account"))
+            {
+                try
+                {
+                    new Account(account);
+                }
+                catch
+                {
+                    Console.WriteLine("Warning: Account instance load failed");
+                }
+            }
+        }
+
+        public static IAccount FindAccount(Serial serial)
+        {
+            _accountsById.TryGetValue(serial, out var account);
+            return account;
         }
     }
-
-    public static void Save()
-    {
-      if (!Directory.Exists("Saves/Accounts"))
-        Directory.CreateDirectory("Saves/Accounts");
-
-      string filePath = Path.Combine("Saves/Accounts", "accounts.xml");
-
-      using StreamWriter op = new StreamWriter(filePath);
-      XmlTextWriter xml = new XmlTextWriter(op) { Formatting = Formatting.Indented, IndentChar = '\t', Indentation = 1 };
-
-      xml.WriteStartDocument(true);
-
-      xml.WriteStartElement("accounts");
-
-      xml.WriteAttributeString("count", m_Accounts.Count.ToString());
-
-      foreach (var account in GetAccounts())
-      {
-          var a = (Account) account;
-          a.Save(xml);
-      }
-
-      xml.WriteEndElement();
-
-      xml.Close();
-    }
-  }
 }
