9ff54843e64c23432fd77b5df23999d0e3a272ce
michaelwhelehan <michael.marula+1@gmail.com>
2021-10-25 22:20:06 +0200

    Fix TriElementalStrike damage

--- ZuluContent/Zulu/Engines/Magic/HitScripts/TriElementalStrike.cs
+++ ZuluContent/Zulu/Engines/Magic/HitScripts/TriElementalStrike.cs
@@ -15,20 +15,20 @@ namespace Server.Engines.Magic.HitScripts
         
         private static async void Cast(Mobile caster, Mobile defender)
         {
-            var damage = SpellHelper.CalcSpellDamage(caster, defender, SpellCircle.Third);
+            var damage = SpellHelper.CalcSpellDamage(caster, defender, 3);
 
             caster.MovingParticles(defender, 0x36D4, 7, 0, false, true, 9502, 4019, 0x160);
             caster.PlaySound(0x44B);
 
             await Timer.Pause(250);
             
-            damage += SpellHelper.CalcSpellDamage(caster, defender, SpellCircle.Third);
+            damage += SpellHelper.CalcSpellDamage(caster, defender, 3);
             
             defender.BoltEffect(0);
             
             await Timer.Pause(250);
 
-            damage += SpellHelper.CalcSpellDamage(caster, defender, SpellCircle.Third);
+            damage += SpellHelper.CalcSpellDamage(caster, defender, 3);
 
             defender.FixedParticles(0x3789, 30, 30, 5028, EffectLayer.Waist);
             defender.PlaySound(0x0116);
