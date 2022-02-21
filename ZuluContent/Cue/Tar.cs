48de22512e06e29c5523ef268726a368cdf785ca
jabin Bastian <this@jabin.id.au>
2022-02-21 23:36:40 +1100

    Hotfix: CUE cli tar.gz files failing due unsupported tar format

--- ZuluContent/Cue/Tar.cs
+++ ZuluContent/Cue/Tar.cs
@@ -5,105 +5,100 @@ using System.Text;
 
 namespace Scripts.Cue
 {
-    public class Tar
-    {
-        /// <summary>
-        /// Extracts a <i>.tar.gz</i> archive to the specified directory.
-        /// </summary>
-        /// <param name="filename">The <i>.tar.gz</i> to decompress and extract.</param>
-        /// <param name="outputDir">Output directory to write the files.</param>
-        public static void ExtractTarGz(string filename, string outputDir)
-        {
-            using (var stream = File.OpenRead(filename))
-                ExtractTarGz(stream, outputDir);
-        }
+    /// <summary>
+    /// Sourced from: https://gist.github.com/ForeverZer0/a2cd292bd2f3b5e114956c00bb6e872b
+    /// </summary>
+	public class Tar
+	{
+		/// <summary>
+		/// Extracts a <i>.tar.gz</i> archive to the specified directory.
+		/// </summary>
+		/// <param name="filename">The <i>.tar.gz</i> to decompress and extract.</param>
+		/// <param name="outputDir">Output directory to write the files.</param>
+		public static void ExtractTarGz(string filename, string outputDir)
+		{
+			using (var stream = File.OpenRead(filename))
+				ExtractTarGz(stream, outputDir);
+		}
 
-        /// <summary>
-        /// Extracts a <i>.tar.gz</i> archive stream to the specified directory.
-        /// </summary>
-        /// <param name="stream">The <i>.tar.gz</i> to decompress and extract.</param>
-        /// <param name="outputDir">Output directory to write the files.</param>
-        public static void ExtractTarGz(Stream stream, string outputDir)
-        {
-            using (var gzip = new GZipStream(stream, CompressionMode.Decompress))
-            {
-                // removed convertation to MemoryStream
-                ExtractTar(gzip, outputDir);
-            }
-        }
+		/// <summary>
+		/// Extracts a <i>.tar.gz</i> archive stream to the specified directory.
+		/// </summary>
+		/// <param name="stream">The <i>.tar.gz</i> to decompress and extract.</param>
+		/// <param name="outputDir">Output directory to write the files.</param>
+		public static void ExtractTarGz(Stream stream, string outputDir)
+		{
+			// A GZipStream is not seekable, so copy it first to a MemoryStream
+			using (var gzip = new GZipStream(stream, CompressionMode.Decompress))
+			{
+				const int chunk = 4096;
+				using (var memStr = new MemoryStream())
+				{
+					int read;
+					var buffer = new byte[chunk];
+                    while ((read = gzip.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        memStr.Write(buffer, 0, read);
+                    }
 
-        /// <summary>
-        /// Extractes a <c>tar</c> archive to the specified directory.
-        /// </summary>
-        /// <param name="filename">The <i>.tar</i> to extract.</param>
-        /// <param name="outputDir">Output directory to write the files.</param>
-        public static void ExtractTar(string filename, string outputDir)
-        {
-            using (var stream = File.OpenRead(filename))
-                ExtractTar(stream, outputDir);
-        }
+                    memStr.Seek(0, SeekOrigin.Begin);
+					ExtractTar(memStr, outputDir);
+				}
+			}
+		}
 
-        /// <summary>
-        /// Extractes a <c>tar</c> archive to the specified directory.
-        /// </summary>
-        /// <param name="stream">The <i>.tar</i> to extract.</param>
-        /// <param name="outputDir">Output directory to write the files.</param>
-        public static void ExtractTar(Stream stream, string outputDir)
-        {
-            var buffer = new byte[100];
-            // store current position here
-            long pos = 0;
-            while (true)
-            {
-                pos += stream.Read(buffer, 0, 100);
-                var name = Encoding.ASCII.GetString(buffer).Trim('\0');
-                if (String.IsNullOrWhiteSpace(name))
-                    break;
-                FakeSeekForward(stream, 24);
-                pos += 24;
+		/// <summary>
+		/// Extractes a <c>tar</c> archive to the specified directory.
+		/// </summary>
+		/// <param name="filename">The <i>.tar</i> to extract.</param>
+		/// <param name="outputDir">Output directory to write the files.</param>
+		public static void ExtractTar(string filename, string outputDir)
+		{
+			using (var stream = File.OpenRead(filename))
+				ExtractTar(stream, outputDir);
+		}
 
-                pos += stream.Read(buffer, 0, 12);
-                var size = Convert.ToInt64(Encoding.UTF8.GetString(buffer, 0, 12).Trim('\0').Trim(), 8);
-                FakeSeekForward(stream, 376);
-                pos += 376;
+		/// <summary>
+		/// Extractes a <c>tar</c> archive to the specified directory.
+		/// </summary>
+		/// <param name="stream">The <i>.tar</i> to extract.</param>
+		/// <param name="outputDir">Output directory to write the files.</param>
+		public static void ExtractTar(Stream stream, string outputDir)
+		{
+			var buffer = new byte[100];
+			while (true)
+			{
+				stream.Read(buffer, 0, 100);
+				var name = Encoding.ASCII.GetString(buffer).Trim('\0');
+				if (String.IsNullOrWhiteSpace(name))
+					break;
+				stream.Seek(24, SeekOrigin.Current);
+				stream.Read(buffer, 0, 12);
+				var size = Convert.ToInt64(Encoding.UTF8.GetString(buffer, 0, 12).Trim('\0').Trim(), 8);
 
-                var output = Path.Combine(outputDir, name);
-                if (!Directory.Exists(Path.GetDirectoryName(output)))
-                    Directory.CreateDirectory(Path.GetDirectoryName(output));
-                if (!name.Equals("./", StringComparison.InvariantCulture))
+				stream.Seek(376L, SeekOrigin.Current);
+
+				var output = Path.Combine(outputDir, name);
+				if (!Directory.Exists(Path.GetDirectoryName(output)))
+					Directory.CreateDirectory(Path.GetDirectoryName(output));
+                if (!name.Equals("./", StringComparison.InvariantCulture)) 
                 {
                     using (var str = File.Open(output, FileMode.OpenOrCreate, FileAccess.Write))
                     {
                         var buf = new byte[size];
-                        pos += stream.Read(buf, 0, buf.Length);
+                        stream.Read(buf, 0, buf.Length);
                         str.Write(buf, 0, buf.Length);
                     }
                 }
 
-                var offset = (int)(512 - (pos % 512));
-                if (offset == 512)
-                    offset = 0;
-                FakeSeekForward(stream, offset);
-                pos += offset;
-            }
-        }
+				var pos = stream.Position;
+	
+				var offset = 512 - (pos  % 512);
+				if (offset == 512)
+					offset = 0;
 
-        private static void FakeSeekForward(Stream stream, int offset)
-        {
-            if (stream.CanSeek)
-                stream.Seek(offset, SeekOrigin.Current);
-            else
-            {
-                int bytesRead = 0;
-                var buffer = new byte[offset];
-                while (bytesRead < offset)
-                {
-                    int read = stream.Read(buffer, bytesRead, offset - bytesRead);
-                    if (read == 0)
-                        throw new EndOfStreamException();
-                    bytesRead += read;
-                }
-            }
-        }
-    }
+				stream.Seek(offset, SeekOrigin.Current);
+			}
+		}
+	}
 }
