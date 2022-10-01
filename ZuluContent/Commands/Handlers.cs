785052f8f64aa7f2676f33611b4c6cff51227340
Pumpkins <crimson-king@gmx.de>
2022-10-01 13:28:26 +0200

    Command prefix is now configurable via settings e.g.: "commandsystem.prefix": "."

--- ZuluContent/Commands/Handlers.cs
+++ ZuluContent/Commands/Handlers.cs
@@ -20,7 +20,7 @@ namespace Server.Commands
     {
         public static void Initialize()
         {
-            CommandSystem.Prefix = "[";
+			CommandSystem.Prefix = ServerConfiguration.GetSetting("commandsystem.prefix", "[");
 
             Register("Go", AccessLevel.Counselor, Go_OnCommand);
 
